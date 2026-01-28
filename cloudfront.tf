ce00374e9ad5100334a6e85ec656d99aa47ec49d
HadyGalal <hadyagalal@gmail.com>
2026-01-28 13:29:13 +0200

    adding files

---
+++ cloudfront.tf
@@ -0,0 +1,34 @@
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name = "s3-oac"
+  description = "Access s3 content via Cloudfront"
+  origin_access_control_origin_type = "s3"
+  signing_behavior = "always"
+  signing_protocol = "sigv4"
+}
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled = true
+  is_ipv6_enabled = true
+  default_root_object = "task-1.html"
+
+  origin {
+    domain_name = aws_s3_bucket.tf-bucket.bucket_regional_domain_name
+    origin_id = "s3-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+  }
+  default_cache_behavior {
+    target_origin_id = "s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+  wait_for_deployment = true
+}
---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = ">=5.0"
+    }
+    random = {
+        source = "hashicorp/random"
+        version = ">=3.5"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+  profile = "tf-admin"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 2
+}
